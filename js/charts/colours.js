d5c8db74a471feac6884a97298f5ff01b307306c
mctipper <morgan.tipper@hotmail.com>
2025-04-19 14:17:38 +1000

    chore: dedicated repo for subpage

---
+++ js/charts/colours.js
@@ -0,0 +1,12 @@
+export const characterColours = {
+    Dixie: 'lightblue',
+    Diddy: 'lightcoral',
+};
+
+
+export const playerColours = {
+    Mog: 'green',
+    Shag: 'purple',
+};
+
+export const successColour = 'gold';
